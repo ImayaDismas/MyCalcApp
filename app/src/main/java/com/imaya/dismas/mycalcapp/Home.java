45acdeb49b3587789f0fb37b5c40a69c3ffb112c
Imaya <imayadismas@gmail.com>
2020-08-16 14:56:15 +0300

    validated the input values to ensure only numeric values are supplied by the user

--- app/src/main/java/com/imaya/dismas/mycalcapp/Home.java
+++ app/src/main/java/com/imaya/dismas/mycalcapp/Home.java
@@ -5,15 +5,20 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import com.imaya.dismas.mycalcapp.databinding.HomeBinding;
 
+import java.util.regex.Pattern;
+
 public class Home extends AppCompatActivity implements View.OnClickListener {
     private static String TAG = Home.class.getSimpleName();
     private HomeBinding binding;
+    private Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,8 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
         binding.btnSubtract.setOnClickListener(this);
         binding.btnDivide.setOnClickListener(this);
         binding.btnMultiply.setOnClickListener(this);
+
+        onTextChangedListener();
     }
 
     @Override
@@ -47,6 +54,85 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
             default:
                 break;
         }
+    }
+
+    private void onTextChangedListener() {
+        binding.teValue1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String input = binding.teValue1.getText().toString().trim();
+                validateValue1(input);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.teValue2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String input = binding.teValue2.getText().toString().trim();
+                validateValue2(input);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private boolean validateValue1(String input){
+        if (TextUtils.isEmpty(input)) {
+            binding.tlValue1.setError("Please input value 1");
+            binding.teValue1.setBackground(ContextCompat.getDrawable(Home.this, R.drawable.error_input_background));
+            return false;
+        } else if (!isNumeric(input)) {
+            binding.tlValue1.setError("Please input valid value 1");
+            binding.teValue1.setBackground(ContextCompat.getDrawable(Home.this, R.drawable.error_input_background));
+            return false;
+        } else {
+            binding.tlValue1.setError(null);
+            binding.tlValue1.setErrorEnabled(false);
+            binding.teValue1.setBackground(ContextCompat.getDrawable(Home.this, R.drawable.input_background));
+            return true;
+        }
+    }
+
+    private boolean validateValue2(String input){
+        if (TextUtils.isEmpty(input)) {
+            binding.tlValue2.setError("Please input value 2");
+            binding.teValue2.setBackground(ContextCompat.getDrawable(Home.this, R.drawable.error_input_background));
+            return false;
+        } else if (!isNumeric(input)) {
+            binding.tlValue2.setError("Please input valid value 2");
+            binding.teValue2.setBackground(ContextCompat.getDrawable(Home.this, R.drawable.error_input_background));
+            return false;
+        } else {
+            binding.tlValue2.setError(null);
+            binding.tlValue2.setErrorEnabled(false);
+            binding.teValue2.setBackground(ContextCompat.getDrawable(Home.this, R.drawable.input_background));
+            return true;
+        }
+    }
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
     }
 
     private void hideKeyboard(){
